f1bed859fb99d248419e40ecb07db8b701bdf80c
cledmir <56932765+cledmir@users.noreply.github.com>
2021-06-02 12:25:48 -0500

    update

--- src/main/java/com/paulsoft/foodyeah/FoodyeahApplication.java
+++ src/main/java/com/paulsoft/foodyeah/FoodyeahApplication.java
@@ -1,5 +1,7 @@
 package com.paulsoft.foodyeah;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodyeahApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FoodyeahApplication.class, args);
+		String urlPage = "https://ajaflorez-coder.github.io/";
+
+		WebDriver webDriver;
+		String pathDriver = "driver\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", pathDriver);
+
+		webDriver = new ChromeDriver();
+		webDriver.get(urlPage);
+
+		String titulo = webDriver.getTitle();
+		System.out.println(titulo);
 	}
 
 }
